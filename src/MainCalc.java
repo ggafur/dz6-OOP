89d745b95fc17b7bf2f9bc314423fbe9843e63b4
Marat <111004274+ggafur@users.noreply.github.com>
2023-02-15 20:03:20 +0300

    Create MainCalc.java

---
+++ src/MainCalc.java
@@ -0,0 +1,11 @@
+import Controller.Calculate;
+
+/**
+ * Рефакторинг и\или оптимизация проекта предыдущего дз с учетом теоретических основ SOLID’а
+ */
+public class MainCalc {
+    public static void main(String[] args) {
+        Calculate calculate = new Calculate();
+        calculate.start();
+    }
+}
